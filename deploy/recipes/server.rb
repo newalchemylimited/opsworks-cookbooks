e485106b9311bae6aebe3453e468a07acf28da45
Anh-Kiet Ngo <anhkiet@gmail.com>
2016-12-21 17:45:52 -0800

    Add restart block

--- deploy/recipes/server.rb
+++ deploy/recipes/server.rb
@@ -23,4 +23,12 @@ node[:deploy].each do |application, deploy|
     environment_variables deploy[:environment_variables]
   end
 
+  ruby_block "restart server #{application}" do
+    block do
+      Chef::Log.info("restart server via: #{node[:deploy][application][:server][:restart_command]}")
+      Chef::Log.info(`#{node[:deploy][application][:server][:restart_command]}`)
+      $? == 0
+    end
+  end
+
 end
